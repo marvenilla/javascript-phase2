bf923dd34d067cec9193b203236074d6a4ec57b0
marvenilla <marvenilla.mindanao@gmail.com>
2024-02-23 19:42:50 -0500

    Add example usage code of the uploadContent method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const UploadContentModule = require('./UploadContentModule');
+
+UploadContentModule.uploadContent(
+    'Review: Winter is Coming',
+    'An insightful review of the first episode of Game of Thrones, discussing the introduction of key characters and setting the scene for the epic saga.',
+    'review',
+    'Detailed review content would go here.'
+);
+
+console.log(UploadContentModule.contentList);
